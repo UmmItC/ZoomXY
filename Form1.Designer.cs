b85e933f03a1d6a515284a8db4207b5c04c8ab23
CsoOBJ1 <undress_undone742@dralias.com>
2023-05-21 21:53:08 +0800

    main source file

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Zoom_XY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._zoomxylabel = new System.Windows.Forms.Label();
+            this._StartBtn = new System.Windows.Forms.Button();
+            this._GetZoomXY = new System.Windows.Forms.Timer(this.components);
+            this._GetZoomJoinXY = new System.Windows.Forms.Timer(this.components);
+            this._zoomjoinlabel = new System.Windows.Forms.Label();
+            this._CheckZoomRunner = new System.Windows.Forms.Timer(this.components);
+            this._zoomaudiolabel = new System.Windows.Forms.Label();
+            this._GetZoomAudioXY = new System.Windows.Forms.Timer(this.components);
+            this._zoomcontentviewXY = new System.Windows.Forms.Label();
+            this._GetZoomContentViewXY = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this._GetZoomLeaveXY = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // _zoomxylabel
+            // 
+            this._zoomxylabel.AutoSize = true;
+            this._zoomxylabel.Font = new System.Drawing.Font("Reem Kufi", 21.75F);
+            this._zoomxylabel.Location = new System.Drawing.Point(12, 9);
+            this._zoomxylabel.Name = "_zoomxylabel";
+            this._zoomxylabel.Size = new System.Drawing.Size(158, 56);
+            this._zoomxylabel.TabIndex = 0;
+            this._zoomxylabel.Text = "Zoom XY : ";
+            // 
+            // _StartBtn
+            // 
+            this._StartBtn.Font = new System.Drawing.Font("Reem Kufi", 21.75F);
+            this._StartBtn.Location = new System.Drawing.Point(22, 327);
+            this._StartBtn.Name = "_StartBtn";
+            this._StartBtn.Size = new System.Drawing.Size(450, 77);
+            this._StartBtn.TabIndex = 1;
+            this._StartBtn.Text = "Start";
+            this._StartBtn.UseVisualStyleBackColor = true;
+            this._StartBtn.Click += new System.EventHandler(this._StartBtn_Click);
+            // 
+            // _GetZoomXY
+            // 
+            this._GetZoomXY.Tick += new System.EventHandler(this._GetZoomXY_Tick);
+            // 
+            // _GetZoomJoinXY
+            // 
+            this._GetZoomJoinXY.Tick += new System.EventHandler(this.GetZoomJoinXY_Tick);
+            // 
+            // _zoomjoinlabel
+            // 
+            this._zoomjoinlabel.AutoSize = true;
+            this._zoomjoinlabel.Font = new System.Drawing.Font("Reem Kufi", 21.75F);
+            this._zoomjoinlabel.Location = new System.Drawing.Point(12, 65);
+            this._zoomjoinlabel.Name = "_zoomjoinlabel";
+            this._zoomjoinlabel.Size = new System.Drawing.Size(213, 56);
+            this._zoomjoinlabel.TabIndex = 2;
+            this._zoomjoinlabel.Text = "Zoom Join XY : ";
+            // 
+            // _CheckZoomRunner
+            // 
+            this._CheckZoomRunner.Tick += new System.EventHandler(this._CheckZoomRunner_Tick);
+            // 
+            // _zoomaudiolabel
+            // 
+            this._zoomaudiolabel.AutoSize = true;
+            this._zoomaudiolabel.Font = new System.Drawing.Font("Reem Kufi", 21.75F);
+            this._zoomaudiolabel.Location = new System.Drawing.Point(12, 121);
+            this._zoomaudiolabel.Name = "_zoomaudiolabel";
+            this._zoomaudiolabel.Size = new System.Drawing.Size(239, 56);
+            this._zoomaudiolabel.TabIndex = 3;
+            this._zoomaudiolabel.Text = "Zoom Audio XY : ";
+            // 
+            // _GetZoomAudioXY
+            // 
+            this._GetZoomAudioXY.Tick += new System.EventHandler(this._GetZoomAudioXY_Tick);
+            // 
+            // _zoomcontentviewXY
+            // 
+            this._zoomcontentviewXY.AutoSize = true;
+            this._zoomcontentviewXY.Font = new System.Drawing.Font("Reem Kufi", 21.75F);
+            this._zoomcontentviewXY.Location = new System.Drawing.Point(12, 177);
+            this._zoomcontentviewXY.Name = "_zoomcontentviewXY";
+            this._zoomcontentviewXY.Size = new System.Drawing.Size(369, 56);
+            this._zoomcontentviewXY.TabIndex = 4;
+            this._zoomcontentviewXY.Text = "Zoom ZPContentView XY : ";
+            // 
+            // _GetZoomContentViewXY
+            // 
+            this._GetZoomContentViewXY.Tick += new System.EventHandler(this._GetZoomContentViewXY_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Reem Kufi", 21.75F);
+            this.label1.Location = new System.Drawing.Point(12, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 56);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Zoom Leave XY : ";
+            // 
+            // _GetZoomLeaveXY
+            // 
+            this._GetZoomLeaveXY.Tick += new System.EventHandler(this.GetZoomLeaveXY_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 520);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._zoomcontentviewXY);
+            this.Controls.Add(this._zoomaudiolabel);
+            this.Controls.Add(this._zoomjoinlabel);
+            this.Controls.Add(this._StartBtn);
+            this.Controls.Add(this._zoomxylabel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label _zoomxylabel;
+        private System.Windows.Forms.Button _StartBtn;
+        private System.Windows.Forms.Timer _GetZoomXY;
+        private System.Windows.Forms.Timer _GetZoomJoinXY;
+        private System.Windows.Forms.Label _zoomjoinlabel;
+        private System.Windows.Forms.Timer _CheckZoomRunner;
+        private System.Windows.Forms.Label _zoomaudiolabel;
+        private System.Windows.Forms.Timer _GetZoomAudioXY;
+        private System.Windows.Forms.Label _zoomcontentviewXY;
+        private System.Windows.Forms.Timer _GetZoomContentViewXY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer _GetZoomLeaveXY;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace Zoom_XY
+{
+    public partial class Form1 : Form
+    {
+        private InputSimulator _input = new InputSimulator(); //Activate KeyBoardControl
+        private MouseSimulator _mouse = new MouseSimulator(); //Activate MouseControl
+        private bool _working = false;
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _CheckZoomRunner.Start();
+            HotKeyManager.RegisterHotKey(Keys.F12, KeyModifiers.Control);
+            HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(_StartBtn_Click);
+        }
+
+        private void _StartBtn_Click(object sender, EventArgs e)
+        {
+            if (_working)
+                StopProgram();
+            else
+                StartProgram();
+        }
+
+        private void StartProgram()
+        {
+            _GetZoomXY.Start();
+            _working = true;
+            _StartBtn.Text = "Started";
+
+            _GetZoomJoinXY.Start();
+
+            _GetZoomAudioXY.Start();
+
+            _GetZoomContentViewXY.Start();
+
+            _GetZoomLeaveXY.Start();
+        }
+
+        private void StopProgram()
+        {
+            _GetZoomXY.Stop();
+            _working = false;
+            _StartBtn.Text = "Stopped";
+
+            _GetZoomJoinXY.Stop();
+            _GetZoomAudioXY.Stop();
+            _GetZoomContentViewXY.Stop();
+            _GetZoomLeaveXY.Stop();
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        private int AbsX(int x)
+        {
+            return x * (65536 / GetSystemMetrics(0));
+        }
+
+        private int AbsY(int y)
+        {
+            return y * (65536 / GetSystemMetrics(0));
+        }
+
+        private void _GetZoomXY_Tick(object sender, EventArgs e)
+        {
+            //Get Zoom XY
+            //Zoom Cloud Meetings
+            var handle = FindWindow("ZPFTEWndClass", "Zoom Cloud Meetings");
+            if (handle != IntPtr.Zero)
+            {
+                var rct = new RECT();
+                GetWindowRect(handle, ref rct);
+
+                var pos = Cursor.Position;
+                pos.X -= rct.Left;
+                pos.Y -= rct.Top;
+                _zoomxylabel.Text = "Zoom XY : " + pos.ToString();
+            }
+        }
+
+        private void GetZoomJoinXY_Tick(object sender, EventArgs e)
+        {
+            //Get Zoom Join XY
+            //Zoom 加入會議 輸入會議密碼
+            var handle = FindWindow("zWaitHostWndClass", null);
+            if (handle != IntPtr.Zero)
+            {
+                var rct = new RECT();
+                GetWindowRect(handle, ref rct);
+
+                var pos = Cursor.Position;
+                pos.X -= rct.Left;
+                pos.Y -= rct.Top;
+                _zoomjoinlabel.Text = "Zoom Join XY : " + pos.ToString();
+            }
+        }
+
+        private void _GetZoomAudioXY_Tick(object sender, EventArgs e)
+        {
+            //zJoinAudioWndClass
+            //加入音訊
+            var handle = FindWindow("zJoinAudioWndClass", null);
+            if (handle != IntPtr.Zero)
+            {
+                var rct = new RECT();
+                GetWindowRect(handle, ref rct);
+
+                var pos = Cursor.Position;
+                pos.X -= rct.Left;
+                pos.Y -= rct.Top;
+                _zoomaudiolabel.Text = "Zoom Audio XY : " + pos.ToString();
+            }
+        }
+        private void _GetZoomContentViewXY_Tick(object sender, EventArgs e)
+        {
+            var handle = FindWindow("ZPContentViewWndClass", null);
+            if (handle != IntPtr.Zero)
+            {
+                var rct = new RECT();
+                GetWindowRect(handle, ref rct);
+
+                var pos = Cursor.Position;
+                pos.X -= rct.Left;
+                pos.Y -= rct.Top;
+                _zoomcontentviewXY.Text = "Zoom ZPContentView XY : " + pos.ToString();
+            }
+        }
+        private void GetZoomLeaveXY_Tick(object sender, EventArgs e)
+        {
+            var handle = FindWindow("SysShadow", null);
+            if (handle != IntPtr.Zero)
+            {
+                //結束會議或離開會議？
+                //zLeaveWndClass
+                var rct = new RECT();
+                GetWindowRect(handle, ref rct);
+
+                var pos = Cursor.Position;
+                pos.X -= rct.Left;
+                pos.Y -= rct.Top;
+                _zoomcontentviewXY.Text = "Zoom Leave XY : " + pos.ToString();
+            }
+        }
+        private void _CheckZoomRunner_Tick(object sender, EventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName("zoom");
+            if (pname.Length == 0)
+            {
+                _StartBtn.Text = "Waiting Zoom Open...";
+                _StartBtn.Enabled = false;
+                _GetZoomXY.Stop();
+                _GetZoomJoinXY.Stop();
+                _zoomxylabel.Text = "Zoom XY : ";
+                _zoomjoinlabel.Text = "Zoom Join XY : ";
+            }
+            else if (pname.Length == 1)
+            {
+                _StartBtn.Text = "Start";
+                _StartBtn.Enabled = true;
+            }
+        }
+    }
+}
